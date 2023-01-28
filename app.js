3648f1c316d736e4698d21815c34d67ddc6ef8e7
xPapashi <dtomkowski1@gmail.com>
2023-01-28 03:32:05 +0000

    Fix grid overflow Fixed grid overflow by removing text from each grid element. Removed 10px padding for each grid as it was overflowing.

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-let removeNumBool = false;
+const DEFAULT_SIZE = 16;
+let removeBorderBool = true;
 
-function createGrid(rows, columns, removeNum) {
+const checkbox = document.querySelector("#checkbox");
+checkbox.onchange = (e) => toggleText(e);
+
+function createGrid(size) {
   const container = document.querySelector(".container-grid");
-  const total = rows * columns;
+  const total = size * size;
+
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
   for (let i = 0; i < total; i++) {
-    container.style.setProperty("--grid-rows", rows);
-    container.style.setProperty("--grid-columns", columns);
-
-    let grid = document.createElement("div");
+    const grid = document.createElement("div");
     grid.classList.add("grid", `grid-${i + 1}`);
-    if (!removeNum) {
-        grid.innerText = `${i + 1}`;
-    } else {
-        grid.style.padding = '10px';
-    }
 
     container.appendChild(grid);
   }
 }
 
 function removeGrid() {
-    const grids = document.querySelectorAll('.grid');
+  const grids = document.querySelectorAll(".grid");
 
-    grids.forEach(grid => {
-        grid.remove();
-    });
+  grids.forEach((grid) => {
+    grid.remove();
+  });
 }
 
 function drawOnGrid() {
@@ -33,38 +32,48 @@ function drawOnGrid() {
 
   grids.forEach((grid) => {
     grid.addEventListener("mouseover", () => {
-    grid.classList.add('gridChange');
+      grid.classList.add("gridChange");
     });
   });
 }
 
-function toggleText() {
-    const checkbox = document.querySelector('#checkbox');
+function toggleText(e) {
+    const grids = document.querySelectorAll('.grid');
 
-    checkbox.addEventListener('change', () => {
-        if (checkbox.checked) {
-            return removeNumBool = false;
-        } else return removeNumBool = true;
-    });
-}
-
-function openPopup() {
-    const button = document.querySelector('.btnChange');
-
-    button.addEventListener('click', () => {
-        let answer = prompt("Enter number of square per side of the grid: e.g. 32");
-
-        if (answer === "" || answer === null) return alert("You need to enter something!");
-        else if (answer > 64) return alert("You cannot enter more than 64!");
-        else {
-            removeGrid();
-            createGrid(answer, answer, removeNumBool);
-            drawOnGrid();
+    grids.forEach((grid) => {
+        console.log(e.target.checked);
+        if (!(e.target.checked)) {
+            grid.style.border = "none";
+            removeBorderBool = true;
+        } else {
+            grid.style.border = "1px solid #000";
+            removeBorderBool = false;
         }
     });
 }
 
-createGrid(16, 16, toggleText());
-drawOnGrid();
-openPopup();
-toggleText();
+function openPopup() {
+  const button = document.querySelector(".btnChange");
+
+  button.addEventListener("click", () => {
+    let answer = prompt("Enter number of square per side of the grid: e.g. 32");
+
+    if (answer === "" || answer === null)
+      return alert("You need to enter something!");
+    else if (answer > 64) return alert("You cannot enter more than 64!");
+    else {
+      removeGrid();
+      createGrid(answer);
+      drawOnGrid();
+    }
+  });
+}
+
+function main() {
+  createGrid(DEFAULT_SIZE);
+  drawOnGrid();
+  openPopup();
+  toggleText();
+}
+
+main();
